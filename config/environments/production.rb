92ab32b2eef41e773ea48f554ea4acdb3b65f52d
Kyle Hurst <kylehurst42@gmail.com>
2015-04-26 21:25:21 -0700

    aws heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
     :storage => :s3,
     :s3_host_name => 's3-us-west-2.amazonaws.com',
     :s3_credentials => {
-      :bucket => 'eleswims',
+      :bucket => ENV['S3_BUCKET'],
       :access_key_id => ENV['S3_KEY'],
       :secret_access_key => ENV['S3_SECRET']
     }
